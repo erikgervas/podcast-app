4c57212e9f9573da43b2fcf0d30d203f5395b863
erikgervas <erikgervas@gmail.com>
2022-09-24 21:43:27 -0300

    Filtering podcast by name or artist is enabled

--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -3,8 +3,16 @@ import podcastService from "../../services/PodcastService";
 import {Podcast} from "./Podcast/Podcast";
 import './Home.css'
 
+const podcastMatchesFilter = (textToFilterBy) => (podcast) => {
+    const textInUpperCase = textToFilterBy.toUpperCase();
+    return textToFilterBy ? (
+        podcast.name.toUpperCase().includes(textInUpperCase) || podcast.artist.toUpperCase().includes(textInUpperCase))
+        : true;
+}
+
 export const Home = () => {
     const [podcasts, setPodcasts] = useState([]);
+    const [textToFilterBy, setTextToFilterBy] = useState('');
 
     useEffect( () => {
         const fetchPodcasts = async () => {
@@ -14,9 +22,22 @@ export const Home = () => {
         fetchPodcasts();
     }, [])
 
+    const onInputChange = (event) => {
+        setTextToFilterBy(event.target.value);
+    };
+
     return(
-      <div className='podcast-list'>
-          { podcasts.map(podcast => <Podcast {...podcast}/>) }
+      <div className='home'>
+          <div className='filter-box'>
+              <div className='badge'>100</div>
+              <input className='filter-input' type="text" placeholder='Filter podcasts...' value={textToFilterBy} onChange={onInputChange}/>
+          </div>
+          <div className='podcast-list'>
+              { podcasts
+                  .filter(podcastMatchesFilter(textToFilterBy))
+                  .map(podcast => <div className='podcast'><Podcast {...podcast}/></div>)
+              }
+          </div>
       </div>
     );
 };
