f8b8bf0f58be78d38572e3500b1c4064fac6d2ef
erikgervas <erikgervas@gmail.com>
2022-10-06 18:49:35 -0300

    Loading on the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import {Link} from "react-router-dom";
 import styles from './Header.module.css'
+import {Loading} from "../Loading/Loading";
 
 export const Header = () => (
     <div className={styles.header}>
         <Link className={styles.link} to="/">Podcaster</Link>
+        <Loading/>
     </div>
 )
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,5 @@
+import './Loading.css'
+
+export const Loading = () => (
+    <span className="loader"/>
+)
